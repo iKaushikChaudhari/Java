a151e326525c69d4b3c05909407ee3578cdd9b59
Kaushik Chaudhari <100680936+iKaushikChaudhari@users.noreply.github.com>
2023-01-17 11:24:09 +0530

    Update SY.java

--- SY.java
+++ SY.java
@@ -5,6 +5,13 @@ public static void main(String arg[])
 int a=50;
 int b=60;
 int Result=a+b;
+
+int sub=a-b;
+int mul=a*b;
+int div=a/b;
 System.out.println("Addition Of Two Number Is: "+Result);
+System.out.println("Substartion Of Two Number Is: "+sub);
+System.out.println("Multiplication Of Two Number Is: "+mul);
+System.out.println("Division Of Two Number Is: "+div);
 }
 }
