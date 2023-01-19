1cab5788833a7cd98627175fa67bf393588a60b3
Kaushik Chaudhari <100680936+iKaushikChaudhari@users.noreply.github.com>
2023-01-19 11:00:19 +0530

    Create Employee.java

---
+++ Employee.java
@@ -0,0 +1,21 @@
+class Employee
+{
+	int eid=265;
+	String name="Kaushik";
+	String city="Pune";
+	String dep="Software";
+	float per=82.82f;
+	char div='B';
+public static void main(String args[])
+	{
+		Employee e1=new Employee();
+		System.out.println("ID Of Employee Is: "+e1.eid);
+		System.out.println("Name Of Employee Is: "+e1.name);
+		System.out.println("City Of Employee Is: "+e1.city);
+		System.out.println("Department Of Employee Is: "+e1.dep);
+		System.out.println("Percentage Of Employee Is: "+e1.per);
+		System.out.println("Divison No Of Employee Is: "+e1.div);
+
+	}
+
+}
