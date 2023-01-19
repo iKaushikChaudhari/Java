c1d96f4c6f706ee8e98b2dbc2f920ac0ee47d70d
Kaushik Chaudhari <100680936+iKaushikChaudhari@users.noreply.github.com>
2023-01-19 10:46:12 +0530

    Add files via upload

---
+++ Student.java
@@ -0,0 +1,20 @@
+class Student
+{
+public static void main(String args[])
+	{
+		int rollno=265;
+		String name="Kaushik";
+		String city="Pune";
+		String dep="Etx";
+		float per=82.82f;
+		char div='B';
+		System.out.println("Roll No Of Student Is: "+rollno);
+		System.out.println("Name Of Student Is: "+name);
+		System.out.println("City Of Student Is: "+city);
+		System.out.println("Department Of Student Is: "+dep);
+		System.out.println("Percentage Of Student Is: "+per);
+		System.out.println("Divison No Of Student Is: "+div);
+
+	}
+
+}
