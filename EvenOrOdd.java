f104e8f54c83775fcc30c33b2d2443ec9311f3bc
Kaushik Chaudhari <100680936+iKaushikChaudhari@users.noreply.github.com>
2023-01-20 14:30:51 +0530

    Add files via upload

---
+++ EvenOrOdd.java
@@ -0,0 +1,17 @@
+package org.mit.engg.ty;
+
+public class EvenOrOdd {
+
+	public static void main(String[] args) {
+		int a=11;
+		if(a%2==0) {
+			System.out.println("Number Is Even");
+		}
+		else {
+			System.out.println("Number Is Odd");
+		}
+		System.out.println("End Of The Program");
+
+	}
+
+}
