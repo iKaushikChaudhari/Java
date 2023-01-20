ffdfeb723c2cab23a4aa4b673febb039f68d1c4c
Kaushik Chaudhari <100680936+iKaushikChaudhari@users.noreply.github.com>
2023-01-20 11:54:27 +0530

    Create DefaultVariables.java

---
+++ DefaultVariables.java
@@ -0,0 +1,30 @@
+package com.mit.engg.etc.sy;
+
+public class DefaultVariables {
+	int a;
+	byte b;
+	boolean b1;
+	float f;
+	char c;
+	short s;
+	double d;
+	long l;
+	String str ;
+	
+	//method to display the data 
+	void displayData()
+	{
+		System.out.println("Int:"+a);
+		System.out.println("byte:"+b);
+		System.out.println("boolean:"+b);
+		System.out.println("float:"+f);
+		System.out.println("char:"+c);
+		System.out.println("short:"+s);
+		System.out.println("double:"+d);
+		System.out.println("long:"+l);
+		System.out.println("string:"+str);
+	}
+	
+	
+
+}
