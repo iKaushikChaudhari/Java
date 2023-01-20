9e4fd8bb6ad4e0c4a8a060b4e5c4d77e040983e0
Kaushik Chaudhari <100680936+iKaushikChaudhari@users.noreply.github.com>
2023-01-20 11:39:12 +0530

    Add files via upload

--- Employee.java
+++ Employee.java
@@ -1,23 +1,30 @@
-class Employee
-{
-	int eid=265;
-	String name="Kaushik";
-	String city="Pune";
-	String dep="Software";
-	float per=82.82f;
-	char div='B';
-public static void main(String args[])
-	{
-		//Object Creation 
-		//creating an object using new keyword 	
-		Employee e1=new Employee();
-		System.out.println("ID Of Employee Is: "+e1.eid);
-		System.out.println("Name Of Employee Is: "+e1.name);
-		System.out.println("City Of Employee Is: "+e1.city);
-		System.out.println("Department Of Employee Is: "+e1.dep);
-		System.out.println("Percentage Of Employee Is: "+e1.per);
-		System.out.println("Divison No Of Employee Is: "+e1.div);
+package org.mit.engg.sy;
 
+import java.util.Scanner;
+
+public class Employee {
+	int eId;
+	String name;
+	String city;
+	Scanner s1=new Scanner(System.in);
+	void getEmployee()
+	{
+		System.out.println("Enter Employee ID :");
+		eId=s1.nextInt();
+		System.out.println("Enter Employee Name :");
+		name=s1.next();
+		System.out.println("Enter Employee City :");
+		city=s1.next();
+	}
+	void display() 
+	{
+		System.out.println("Employee ID :"+eId);
+		System.out.println("Employee Name :"+name);
+		System.out.println("Employee City :"+city);
 	}
 
+	public static void main(String args[]) 
+	{
+
+	}
 }
