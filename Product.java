0187f1220f0e885277f0786c713e4333027fecb3
Kaushik Chaudhari <100680936+iKaushikChaudhari@users.noreply.github.com>
2023-01-20 12:20:46 +0530

    Day03

--- Product.java
+++ Product.java
@@ -1,28 +1,18 @@
-import java.util.Scanner;
-class Product
-{
-public static void main(String args[])
-{
-	int pId;
+package org.mit.engg.sy;
+
+public class Product {
+	int pid;
 	String pName;
 	float pPrice;
-	
-	//Product p1=new Product();
-//Declare the object and initialize with
-//predefined standard input object
-	Scanner s1=new Scanner(System.in);
-	System.out.println("Enter Product ID: ");
-	//Numerical input
-	pId=s1.nextInt();
-	System.out.println("Enter Product Name: ");
-	//String input
-	pName=s1.next();
-	System.out.println("Enter Product Price: ");
-	//float input
-	pPrice=s1.nextFloat();
-	//Output
-	System.out.println("Product ID Is: "+pId);
-	System.out.println("Product Name Is: "+pName);
-	System.out.println("Product Name Is: "+pPrice);
-}
+	void getDetails(int pid,String pName,float pPrice) {
+		this.pid=pid;
+		this.pName=pName;
+		this.pPrice=pPrice;
+	}
+	void displayProductDetails() {
+		System.out.println("Product ID is "+pid);
+		System.out.println("Product Name is "+pName);
+		System.out.println("Product Price is "+pPrice);
+	}
+
 }
