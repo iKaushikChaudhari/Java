8ebc2f18356d233f0d0a5d85bfe3720ccc962233
Kaushik Chaudhari <100680936+iKaushikChaudhari@users.noreply.github.com>
2023-01-20 14:24:03 +0530

    Day03

---
+++ DowhileLoop.java
@@ -0,0 +1,15 @@
+package org.mit.engg.ty;
+
+public class DowhileLoop {
+
+	public static void main(String[] args) {
+		int a=1;
+		do {
+			System.out.println("Hello");
+			a++;
+		} while (a>2);
+		System.out.println("End Of Program");
+
+	}
+
+}
---
+++ Test.java
@@ -0,0 +1,15 @@
+package org.mit.engg.ty;
+
+public class Test {
+
+	public static void main(String[] args) {
+		int a=1;
+		while(a<=3) {
+			System.out.println("Hello");
+			a++;
+		}
+		System.out.println("End of block");
+
+	}
+
+}
