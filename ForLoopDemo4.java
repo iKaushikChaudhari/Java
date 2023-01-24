804b8f290dbe1313b8983c2d2695dab35ca1566c
Kaushik Chaudhari <100680936+iKaushikChaudhari@users.noreply.github.com>
2023-01-24 10:23:59 +0530

    Add files via upload

---
+++ ForLoopDemo4.java
@@ -0,0 +1,16 @@
+package Day05.java;
+//Day05
+public class ForLoopDemo4 {
+
+	public static void main(String[] args) {
+		for(int a=1;a<=100;a++) {
+			if(a%2==0) {
+				System.out.println(a);
+			}
+			//System.out.println(a);
+		}
+		System.out.println("End Of The Program");
+
+	}
+
+}
