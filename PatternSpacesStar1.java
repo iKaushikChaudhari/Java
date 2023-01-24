5581c4065519e6edb1c51c7746c1186a064fa57d
Kaushik Chaudhari <100680936+iKaushikChaudhari@users.noreply.github.com>
2023-01-24 11:15:46 +0530

    Add files via upload

---
+++ PatternSpacesStar1.java
@@ -0,0 +1,26 @@
+package Day05.java;
+/*
+    *
+   **
+  ***
+ ****
+*****
+ */
+public class PatternSpacesStar1 {
+
+	public static void main(String[] args) {
+		for(int i=1;i<=5;i++) {
+			for(int j=4;j>=i;j--) {
+				System.out.print(" ");
+
+			}
+			for(int k=1;k<=i;k++) {
+				System.out.print("*");
+			}
+			//System.out.print("*");
+			System.out.println();
+		}
+
+	}
+
+}
