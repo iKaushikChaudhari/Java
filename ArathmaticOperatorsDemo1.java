9f4c3e5971e9cb8b9a8df4a1eed96fdd95ca88e0
Kaushik Chaudhari <100680936+iKaushikChaudhari@users.noreply.github.com>
2023-01-24 11:57:54 +0530

    Day05

---
+++ ArathmaticOperatorsDemo1.java
@@ -0,0 +1,32 @@
+package Day05.java;
+
+public class ArathmaticOperatorsDemo1 {
+	int a,b,Result;
+	void getData(int a,int b)
+	{
+		this.a=a;
+		this.b=b;
+	}
+	void add()
+	{
+		Result=a+b;
+		System.out.println("Add is "+Result);
+	}
+	void sub()
+	{
+		Result=a-b;
+		System.out.println("Sub is "+Result);
+	}
+	void mul()
+	{
+		Result=a*b;
+		System.out.println("Mul is "+Result);
+	}
+	void div()
+	{
+		Result=a/b;
+		System.out.println("Div is "+Result);
+	}
+
+
+}
