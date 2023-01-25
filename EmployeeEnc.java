009bded3a996f01eda004be6af218886ced2ffd1
Kaushik Chaudhari <100680936+iKaushikChaudhari@users.noreply.github.com>
2023-01-25 12:03:35 +0530

    Create EmployeeEnc.java

---
+++ EmployeeEnc.java
@@ -0,0 +1,62 @@
+
+public class EmployeeEnc {
+	private int eId;
+	private String eName;
+	private String eCity;
+	private float eSal;
+	
+	public EmployeeEnc() {
+		
+	}
+	static {
+		
+	}
+	
+	{
+		
+	}
+
+	public void getData(int eId,String eName,String eCity,float eSal) {
+		this.eId=eId;
+		this.eName=eName;
+		this.eCity=eCity;
+		this.eSal=eSal;
+	}
+	public void displayData() {
+		System.out.println(eId);
+		System.out.println(eName);
+		System.out.println(eCity);
+		System.out.println(eSal);
+
+	}
+	
+	public void seteId(int eId) {
+		this.eId=eId;
+	}
+	public int geteId() {
+		return eId;
+	}
+	
+	public void seteName(String eName) {
+		this.eName=eName;
+	}
+	public String geteName() {
+		return eName;
+	}
+	
+	public String geteCity() {
+		return eCity;
+	}
+	public void seteCity(String eCity) {
+		this.eCity = eCity;
+	}
+	
+	public float geteSal() {
+		return eSal;
+	}
+	public void seteSal(float eSal) {
+		this.eSal = eSal;
+	}
+	
+
+}
